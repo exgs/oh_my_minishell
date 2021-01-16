ba4b9be1d62c59ac17c5e11065d0213b4ae29ee8
Ykoh <ykoh@student.42seoul.kr>
2021-01-17 01:50:41 +0900

    Fix : error message 127 to 2(incorrect usage)

--- oh_my_minishell/execute_env.c
+++ oh_my_minishell/execute_env.c
@@ -1,6 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute_env.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykoh <ykoh@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/17 01:49:01 by ykoh              #+#    #+#             */
+/*   Updated: 2021/01/17 01:49:04 by ykoh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-static void	export_env_path(const char *path, char *const argv[], char *const envp[])
+static int	msg_incorrect(char *arg)
+{
+	ft_putstr_fd("bash: env: ", 2);
+	ft_putstr_fd(arg, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putendl_fd("incorrect usage", 2);
+	ft_putendl_fd("usage: env without any options and any arguments", 2);
+	g_status = 2 * 256;
+	return (-1);
+}
+
+static void	export_env_path(const char *path,
+							char *const argv[], char *const envp[])
 {
 	char	*tmp[3];
 
@@ -14,21 +38,15 @@ static void	export_env_path(const char *path, char *const argv[], char *const en
 	free(tmp[1]);
 }
 
-int		execute_env(const char *path, char *const argv[], char *const envp[])
+int			execute_env(const char *path,
+						char *const argv[], char *const envp[])
 {
-
 	int		i;
 	int		j;
 	char	**tmp;
-	if (argv[1] != NULL)
-	{
-		g_status = 256 * 127;
-		ft_putstr_fd("env: ", 2);
-		ft_putstr_fd(argv[1], 2);
-		ft_putendl_fd(": No such file or directory", 2);
-		return (-1);
-	}
 
+	if (argv[1])
+		return (msg_incorrect(argv[1]));
 	export_env_path(path, argv, get_param()->envp);
 	i = 0;
 	while (envp[i])
@@ -47,5 +65,4 @@ int		execute_env(const char *path, char *const argv[], char *const envp[])
 	ft_putendl_fd(envp[j], 1);
 	g_status = 0;
 	return (0);
-	(void)argv;
 }
