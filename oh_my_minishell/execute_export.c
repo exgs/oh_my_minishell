bf4ea5c3f10c8d99761e41eb9669631ec8b8eb4a
Ykoh <ykoh@student.42seoul.kr>
2021-01-20 14:47:43 +0900

    Fix : export aa = b error

--- oh_my_minishell/execute_export.c
+++ oh_my_minishell/execute_export.c
@@ -6,7 +6,7 @@ static int	is_valid(char str[])
 	int		end;
 	char	*eq;
 
-	if (ft_isdigit(str[0]))
+	if (ft_isdigit(str[0]) || str[0] == '=')
 		return (0);
 	if ((eq = ft_strchr(str, '=')))
 		end = eq - str;
