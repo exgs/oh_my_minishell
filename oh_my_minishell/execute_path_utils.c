f7cd52d99bbc375a6389fa815d930daa41a4d747
yunslee <yunafrica@gmail.com>
2021-01-21 16:52:11 +0900

    Norm: checkout_command_utils.c / execute_path_utils.c

--- oh_my_minishell/execute_path_utils.c
+++ oh_my_minishell/execute_path_utils.c
@@ -6,13 +6,13 @@
 /*   By: yunslee <yunslee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 03:11:30 by yunslee           #+#    #+#             */
-/*   Updated: 2021/01/21 03:20:37 by yunslee          ###   ########.fr       */
+/*   Updated: 2021/01/21 16:43:23 by yunslee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int command_not_found_g_status(char *str)
+int		command_not_found_g_status(char *str)
 {
 	ft_putendl_fd(str, 2);
 	ft_putstr_fd("minishell: ", 2);
@@ -22,7 +22,7 @@ int command_not_found_g_status(char *str)
 	return (0);
 }
 
-void copy_pathname(char *str, char *buff)
+void	copy_pathname(char *str, char *buff)
 {
 	int i;
 
@@ -35,7 +35,7 @@ void copy_pathname(char *str, char *buff)
 	}
 }
 
-int is_macro_in_envp(const char *env_line, char *buff)
+int		is_macro_in_envp(const char *env_line, char *buff)
 {
 	if (ft_strncmp(env_line, buff, ft_strlen(buff) + 1) == '=')
 		return (TRUE);
