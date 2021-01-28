7b851c8c9d92a71d7f33e365c30bdf3982360d08
PennyBlack2008 <g2ang12@gmail.com>
2021-01-28 20:49:03 +0900

    arranged little in backslash

--- oh_my_minishell/refine_line_b.c
+++ oh_my_minishell/refine_line_b.c
@@ -6,7 +6,7 @@
 /*   By: jikang <jikang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 01:40:53 by jikang            #+#    #+#             */
-/*   Updated: 2021/01/28 20:40:05 by jikang           ###   ########.fr       */
+/*   Updated: 2021/01/28 20:45:43 by jikang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,17 @@
 
 static void	back_slash(char *buff, char *line, t_var *v)
 {
-	if (v->flag_bq == 0 && line[(v->i) + 1] != '\0')
-		(v->i)++;
-	if (v->flag_bq == 1 && line[(v->i) + 1] == '"')
-		(v->i)++;
-	else if (v->flag_bq == 1 && line[(v->i) + 1] == '\\')
-		(v->i)++;
-	else if (line[(v->i) + 1] == '$')
-		(v->i)++;
+	if (line[(v->i) + 1] != '\0')
+	{
+		if (v->flag_bq == 0)
+			(v->i)++;
+		else if (v->flag_bq == 1 && line[(v->i) + 1] == '"')
+			(v->i)++;
+		else if (v->flag_bq == 1 && line[(v->i) + 1] == '\\')
+			(v->i)++;
+		else if (line[(v->i) + 1] == '$')
+			(v->i)++;
+	}
 	buff[v->k] = line[v->i];
 	(v->k)++;
 }
